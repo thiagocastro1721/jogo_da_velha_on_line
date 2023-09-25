4d994f1d1c1cfb21db7dd5bec76e0c5566cccd4a
thiagocastro1721 <58869140+thiagocastro1721@users.noreply.github.com>
2023-09-25 02:10:37 -0300

    Visibilidade da senha e tela de casdastro.

--- lib/pages/register.page.dart
+++ lib/pages/register.page.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/pages/my_game_page.dart';
-import 'package:my_app/pages/register.page.dart';
 
 // Define a custom Form widget.
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
+class MyRegisterForm extends StatefulWidget {
+  const MyRegisterForm({super.key});
 
   @override
-  State<MyCustomForm> createState() => _MyCustomFormState();
+  State<MyRegisterForm> createState() => _MyFormState();
 }
 
 // Define a corresponding State class.
 // This class holds data related to the form.
-class _MyCustomFormState extends State<MyCustomForm> {
+class _MyFormState extends State<MyRegisterForm> {
   // Define the focus node. To manage the lifecycle, create the FocusNode in
   // the initState method, and clean it up in the dispose method.
   late FocusNode myFocusNode;
   FocusNode fieldOne = FocusNode();
   FocusNode fieldTwo = FocusNode();
+  FocusNode fieldThree = FocusNode();
+  FocusNode fieldFour = FocusNode();
+  FocusNode fieldFive = FocusNode();
 
   bool _showPassword = false;
+  bool _confirmShowPassword = false;
 
   @override
   void initState() {
@@ -55,14 +58,35 @@ class _MyCustomFormState extends State<MyCustomForm> {
               ), // Caminho para a imagem no diretório de assets
             ),
             const Text(
-              'Jogo da Velha',
+              'Cadastro',
               style: TextStyle(fontSize: 16),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
               child: TextFormField(
                   autofocus: true,
                   focusNode: fieldOne,
+                  decoration: InputDecoration(
+                    icon: const Icon(
+                      Icons.account_circle_sharp,
+                      color: Colors.blue,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          const BorderSide(color: Colors.grey, width: 2.0),
+                    ),
+                    labelText: 'Apelido',
+                  ),
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(fieldTwo);
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: TextFormField(
+                  autofocus: true,
+                  focusNode: fieldTwo,
                   decoration: InputDecoration(
                     icon: const Icon(
                       Icons.alternate_email,
@@ -76,14 +100,35 @@ class _MyCustomFormState extends State<MyCustomForm> {
                     labelText: 'E-mail',
                   ),
                   onFieldSubmitted: (value) {
-                    FocusScope.of(context).requestFocus(fieldTwo);
+                    FocusScope.of(context).requestFocus(fieldThree);
+                  }),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: TextFormField(
+                  autofocus: true,
+                  focusNode: fieldThree,
+                  decoration: InputDecoration(
+                    icon: const Icon(
+                      Icons.alternate_email,
+                      color: Colors.blue,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                      borderSide:
+                          const BorderSide(color: Colors.grey, width: 2.0),
+                    ),
+                    labelText: 'Confirme o e-mail',
+                  ),
+                  onFieldSubmitted: (value) {
+                    FocusScope.of(context).requestFocus(fieldFour);
                   }),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
               child: TextFormField(
                 //autofocus: true,
-                focusNode: fieldTwo,
+                focusNode: fieldFour,
                 obscureText: _showPassword == false ? true : false,
                 decoration: InputDecoration(
                   icon: const Icon(
@@ -110,11 +155,46 @@ class _MyCustomFormState extends State<MyCustomForm> {
                     },
                   ),
                 ),
+              onFieldSubmitted: (value) {
+                FocusScope.of(context).requestFocus(fieldFive);
+              }
               ),
             ),
             Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              child: TextFormField(
+                //autofocus: true,
+                focusNode: fieldFive,
+                obscureText: _confirmShowPassword == false ? true : false,
+                decoration: InputDecoration(
+                  icon: const Icon(
+                    Icons.lock,
+                    color: Colors.blue,
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 2.0),
+                  ),
+                  labelText: 'Confirme a senha',
+                  suffixIcon: GestureDetector(
+                    child: Icon(
+                      _confirmShowPassword == false
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                      color: Colors.blue,
+                    ),
+                    onTap: () {
+                      setState(() {
+                        _confirmShowPassword = !_confirmShowPassword;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                 child: ElevatedButton(
                   onPressed: () {
                     // Função a ser executada quando o botão for pressionado
@@ -136,16 +216,10 @@ class _MyCustomFormState extends State<MyCustomForm> {
                   ),
                 )),
             Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
                 child: ElevatedButton(
                   onPressed: () {
                     // Função a ser executada quando o botão for pressionado
-                    Navigator.of(context).pushReplacement(
-                      MaterialPageRoute(
-                        builder: ((context) => const MyRegisterForm()),
-                      ),
-                    );
                   },
                   style: ElevatedButton.styleFrom(
                     padding: const EdgeInsets.all(
